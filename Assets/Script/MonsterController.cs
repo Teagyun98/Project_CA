18acfaafac87e2b11a1158e9f70e8edac9bf3a83
김태균 <bbac2422@gmail.com>
2024-04-25 16:38:13 +0900

    24/04/25

--- Assets/Script/MonsterController.cs
+++ Assets/Script/MonsterController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 
 public enum MonsterState
@@ -15,14 +17,26 @@ public class MonsterIdle : CharState<MonsterController>
 {
     MonsterController controller;
 
+    private float attackDelay;
+
     public void OperateEnter(MonsterController sender) 
     {
         controller = sender;
-
+        attackDelay = controller.AttackDelay;
         controller.Animator.SetBool("Idle", true);
     }
     public void OperateExit(MonsterController sender) { controller.Animator.SetBool("Idle", false); }
-    public void OperateUpdate(MonsterController sender) { }
+    public void OperateUpdate(MonsterController sender) 
+    {
+        attackDelay -= Time.deltaTime;
+
+        if (controller.target != null && Vector2.Distance(controller.transform.position, controller.target.transform.position) < controller.AttackDistance)
+        {
+            controller.Sm.SetState(controller.DicState[MonsterState.Attack]);
+            attackDelay = GameManager.Instance.monsterAttackDelay;
+        }
+
+    }
     public void OperateFixedUpdate(MonsterController sender) { }
 }
 
@@ -44,11 +58,6 @@ public class MonsterRun : CharState<MonsterController>
             return;
 
         controller.transform.position = Vector2.MoveTowards(controller.transform.position, controller.target.transform.position, 1 * Time.fixedDeltaTime);
-
-        if (Vector2.Distance(controller.transform.position, controller.target.transform.position) < controller.AttackDistance)
-        {
-            controller.Sm.SetState(controller.DicState[MonsterState.Attack]);
-        }
     }
 }
 
@@ -94,8 +103,8 @@ public class MonsterController : MonoBehaviour
     public float AttackDelay { get; private set; }
     public float AttackDistance { get; private set; }
 
-    private List<CharController> targetList;
-    public CharController target { get; private set; }
+    public List<CharController> TargetList { get; private set; }
+    public CharController target;
 
     private void Awake()
     {
@@ -104,7 +113,7 @@ public class MonsterController : MonoBehaviour
 
     private void Start()
     {
-        targetList = new List<CharController>();
+        TargetList = new List<CharController>();
 
         MaxHp = GameManager.Instance.monsterHp;
         AttackPower = GameManager.Instance.monsterAttackPower;
@@ -129,13 +138,13 @@ public class MonsterController : MonoBehaviour
 
     private void Update()
     {
-        foreach(CharController ch in targetList) 
+        foreach (CharController ch in TargetList)
         {
             if (target == null || Vector2.Distance(transform.position, target.transform.position) > Vector2.Distance(transform.position, ch.transform.position))
                 target = ch;
         }
 
-        if (targetList.Count == 0)
+        if (TargetList.Count == 0)
             target = null;
 
         Sm.DoOperateUpdate();
@@ -152,11 +161,8 @@ public class MonsterController : MonoBehaviour
         {
             CharController target = collision.transform.GetComponent<CharController>();
 
-            if(targetList.Contains(target) == false)
-                targetList.Add(target);
-
-            if (targetList.Count > 0)
-                Sm.SetState(DicState[MonsterState.Run]);
+            if(TargetList.Contains(target) == false)
+                TargetList.Add(target);
         }
     }
 
@@ -167,11 +173,8 @@ public class MonsterController : MonoBehaviour
         {
             CharController target = collision.transform.GetComponent<CharController>();
 
-            if (targetList.Contains(target) == true)
-                targetList.Remove(target);
-
-            if (targetList.Count == 0)
-                Sm.SetState(DicState[MonsterState.Idle]);
+            if (TargetList.Contains(target) == true)
+                TargetList.Remove(target);
         }
     }
 
